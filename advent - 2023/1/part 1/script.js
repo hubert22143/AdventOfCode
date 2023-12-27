9b003160879ff74f0fdb4d8a65c91e88ad6d6baa
hubert22143 <33151182+hubert22143@users.noreply.github.com>
2023-12-27 22:50:23 +0100

    ex 1 - part 1

---
+++ advent - 2023/1/part 1/script.js
@@ -0,0 +1,43 @@
+const fileInput = document.getElementById('fileInput');
+let valuesToRetrieve = [];
+
+fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            valuesToRetrieve = e.target.result.split('\n');
+            valuesToRetrieve = valuesToRetrieve.filter(value => value.trim() !== '');
+            console.log('Values from the file:', valuesToRetrieve);
+            processValues();
+        };
+        reader.readAsText(file);
+    }
+});
+
+const valuesToCheck = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function extractDigits(input) {
+    const allDigits = input.map(value => {
+        const matches = value.match(/\d/g);
+        return matches ? matches.join('') : '';
+    });
+    return allDigits.filter(value => value !== '');
+}
+
+function processValues() {
+    let totalSum = 0;
+    let extractedValues = extractDigits(valuesToRetrieve);
+    extractedValues.forEach(extractedValue => {
+        if (extractedValue) {
+            let firstNumber = parseInt(extractedValue[0]);
+            let lastNumber = parseInt(extractedValue.slice(-1));
+            let arr = parseInt([firstNumber,lastNumber].join(''));
+            console.log("The numbers are:",arr);
+            totalSum += arr
+
+        }
+    });
+
+    console.log('Total Sum:', totalSum);
+}
